d287335130304e658410e8dc7a1e002262858937
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-03-21 01:10:56 +0300

    refac: improvement v1

--- QR_code/qr_code.py
+++ QR_code/qr_code.py
@@ -1,12 +1,22 @@
 import qrcode
 
-website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+def generate_qr_code(website_link, box_size=5, border=5, fill_color='black', back_color='white', file_name='qr_code.png'):
+    qr = qrcode.QRCode(version=1, box_size=box_size, border=border)
+    qr.add_data(website_link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=fill_color, back_color=back_color)
+    img.save(file_name)
+    print(f"QR code generated for '{website_link}' and saved as '{file_name}'")
 
-qr = qrcode.QRCode(version=1, box_size=5, border=5)
+def main():
+    website_link = input("Enter the website link: ")
+    box_size = int(input("Enter the box size (default is 5): ") or 5)
+    border = int(input("Enter the border size (default is 5): ") or 5)
+    fill_color = input("Enter the fill color (default is 'black'): ") or 'black'
+    back_color = input("Enter the background color (default is 'white'): ") or 'white'
+    file_name = input("Enter the file name to save the QR code (default is 'qr_code.png'): ") or 'qr_code.png'
 
-qr.add_data(website_link)
-qr.make(fit=True)
+    generate_qr_code(website_link, box_size, border, fill_color, back_color, file_name)
 
-img = qr.make_image(fill='black', back_color='white')
-
-img.save('youtube_qr.png')
+if __name__ == "__main__":
+    main()
